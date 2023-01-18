817cb104fecf0584472ffe131aa245b19df7993f
abdelhay bouramdane <109427562+Abdebouramdane@users.noreply.github.com>
2023-01-18 21:03:47 +0100

    abdelhay bouramdane

--- Day 004/exercie 2/main.py
+++
@@ -1,4 +0,0 @@
-name = ["Delaware","Pennsylvania","New Jersey","Georgia","Connecticut","Massachusetts","Maryland","South Carolina","New Hampshire","Virginia","New York","North Carolina","Rhode Island","Vermont","Kentucky","Tennessee","Ohio","Louisiana","IndianaD","MississippiD","Illinois","AlabamaDe","Maine","Missouri","Arkansas","Michigan","Florida","TexasD","IowaD","Wisconsin","CaliforniaS","Minnesota","OregonF","Kansas","West Virginia","Nevada","Nebraska","District of ColombiaF","Colorado","North Dakota","South Dakota","Montana","WashingtonN","Idaho","Wyoming","Utah","OklahomaN","New Mexico","Arizona","Alaska","Hawaii"]
-
-for i in range(0, len(name)): 
-    print(name[-i - 1])

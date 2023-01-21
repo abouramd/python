ef8be0242d5d46d5f2e2cb219ee35f19ce8cc117
abdelhay bouramdane <109427562+Abdebouramdane@users.noreply.github.com>
2023-01-21 03:28:27 +0100

    @abouramd

---
+++ Day 006/exercie 1/Alone.py
@@ -0,0 +1,18 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def move_right():
+    move()
+    turn_right()
+    
+turn_left()
+move_right()
+move_right()
+move_right()
+move()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################

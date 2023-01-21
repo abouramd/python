a4f21ced6960884585df379afaef257c8dc9554c
abdelhay bouramdane <109427562+Abdebouramdane@users.noreply.github.com>
2023-01-21 01:13:34 +0100

    @abouramd

--- Day 002/exercie 4/main.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the tip calculator.")
-bill = float(input("What was the total bill? $"))
-pertip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill? "))
-tip = '{:.2f}'.format((bill / 100 * pertip + bill) / people, 2)
-print(f"Each person should pay: ${tip}")
--- Day 005/exercie 3/main.py
+++
@@ -1,7 +0,0 @@
-#Write your code below this row 👇
-
-sum = 0
-for i in range(2, 101):
-    if i % 2 == 0:
-        sum += i
-print(sum)

e1d0ee663fb7f03d997d5b9b849ca89b79475ca4
abdelhay bouramdane <109427562+abouramd@users.noreply.github.com>
2023-02-12 18:05:54 +0000

    @abouramd

---
+++ Day 007/exercie 1/main.py
@@ -0,0 +1,22 @@
+import random
+import copy
+
+#Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+str = word_list[random.randint(0, 2)]
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+c = input("guess a char ?")
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+
+for i in str:
+    if i == c:
+        print("Right")
+    else:
+        print("Wrong")
